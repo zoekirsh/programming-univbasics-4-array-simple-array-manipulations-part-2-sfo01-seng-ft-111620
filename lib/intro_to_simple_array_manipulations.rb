aa8e7ca1f5d008a07a8a9d901ccb2d60d87ebe44
maxwellbenton <maxwellbenton@gmail.com>
2020-02-22 19:37:11 -0500

    remove part 1 solution

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,27 +1,3 @@
-def using_push(array, string)
-  array.push(string)
-end
-
-def using_unshift(array, string)
-  array.unshift(string)
-end
-
-def using_pop(array)
-  array.pop
-end
-
-def pop_with_args(array)
-  array.pop(2)
-end
-
-def using_shift(array)
-  array.shift
-end
-
-def shift_with_args(array)
-  array.shift(2)
-end
-
 def using_concat(array1, array2)
   array1.concat(array2)
 end
